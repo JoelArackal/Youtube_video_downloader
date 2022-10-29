d64d197a59b19c9a80f1c9cf9b3b9ea74d421e51
Joel Jose <joeljosearackal@gmail.com>
2022-10-29 23:00:10 +0530

    first commit

---
+++ basic/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, HttpResponse
+from pytube import YouTube
+
+# Create your views here.
+
+
+def index(request):
+    url = 'https://www.youtube.com/watch?v=c45fVxBnUFs'
+    yt = YouTube(url)
+    vids = yt.streams.all()
+    print(vids[0])
+    return HttpResponse(f'<h1>Hello {vids[0].title} </h1>')
---
+++ procfile
@@ -0,0 +1,1 @@
+web: gunicorn YTDownloader.wsgi --log-file -
