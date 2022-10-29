db5aa5e6a9e80563d78b2d86cf3cdcf7aaa320cb
Joel Jose <joeljosearackal@gmail.com>
2022-10-30 00:03:40 +0530

    second commit

--- basic/views.py
+++ basic/views.py
@@ -5,8 +5,13 @@ from pytube import YouTube
 
 
 def index(request):
-    url = 'https://www.youtube.com/watch?v=c45fVxBnUFs'
-    yt = YouTube(url)
-    vids = yt.streams.all()
-    print(vids[0])
-    return HttpResponse(f'<h1>Hello {vids[0].title} </h1> <a href="{vids[2].url}">link</a>')
+    if request.method == 'POST':
+        url = request.POST['url']
+        print(url)
+        yt = YouTube(url)
+        vids = yt.streams.all()
+        print(vids[0])
+        y_url = vids[2].url
+        return render(request, 'videos.html',{'url': y_url}) 
+    # return HttpResponse(f'<h1>Hello {vids[0].title} </h1> <a href="{vids[2].url}">link</a>')
+    return render(request, 'index.html')
